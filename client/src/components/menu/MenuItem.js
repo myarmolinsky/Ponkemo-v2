9122b326810471fe95b8bcde9e06839ab32aaf9c
myarmolinsky <myarmolinsky3043@bths.edu>
2021-02-10 16:20:54 -0500

    Styled MenuItem

--- client/src/components/menu/MenuItem.js
+++ client/src/components/menu/MenuItem.js
@@ -3,7 +3,11 @@ import { string } from "prop-types";
 
 export const MenuItem = ({ text, color }) => {
   return (
-    <button className="menu-item" style={{ background: color }}>
+    <button
+      className="menu-item"
+      style={{ background: color }}
+      onClick={() => console.log(text)}
+    >
       {text}
     </button>
   );
