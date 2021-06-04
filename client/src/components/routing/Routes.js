bfe473043eb65a89738db5d2c2f0a04d974886db
myarmolinsky <yarmolinskymatthew@gmail.com>
2021-06-04 15:55:09 -0400

    Created ForgotPassword page

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { Register } from "../auth/Register";
-import { Login } from "../auth/Login";
+import { Register, Login, ForgotPassword } from "../auth";
 import { Menu } from "../menu/Menu";
 import {
   Pokedex,
@@ -26,6 +25,7 @@ const Routes = () => {
         {/*wrap everything in a switch so we don't have issues, especially when we create our 'private route' component*/}
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/forgot-password" component={ForgotPassword} />
         <PrivateRoute exact path="/menu" component={Menu} />
         <PrivateRoute exact path="/pc" component={PC} />
         <PrivateRoute exact path="/profile" component={Profile} />
